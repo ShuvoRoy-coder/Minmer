7b5fe0e7fe7105e18bb558810399e92c7a29b8ff
ShuvoRoy-coder <sroy90677@gmail.com>
2025-06-14 03:47:19 +0600

    updated

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -11,7 +11,7 @@ const page = usePage();
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
-    <div class="">
+    <div class="w-full">
 
         <Navbar/>
 
